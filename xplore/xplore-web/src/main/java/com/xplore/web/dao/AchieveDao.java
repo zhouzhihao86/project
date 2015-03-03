e335a625051b2cada0d442110c214d3f0cf721f9
zhouzhihao86 <zhouzhihao86@outlook.com>
2015-03-03 14:25:20 +0800

    test

--- xplore/xplore-web/src/main/java/com/xplore/web/dao/AchieveDao.java
+++ xplore/xplore-web/src/main/java/com/xplore/web/dao/AchieveDao.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Repository;
 public class AchieveDao extends HibernateBaseDao<Achieve, Integer> {
     public Page pagedList(Page<Achieve> page) {
 
-        String hql = "from Achieve order by id desc";
+        String hql = "from Achieve order by id desc1";
         return find(page, hql);
 
     }
