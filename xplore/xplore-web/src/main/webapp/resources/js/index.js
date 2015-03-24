c081e47c6d07b5f211171c950e8f1d9b512daeb0
zhouzhihao86 <zhouzhihao86@outlook.com>
2015-03-24 16:59:18 +0800

    345354

--- xplore/xplore-web/src/main/webapp/resources/js/index.js
+++ xplore/xplore-web/src/main/webapp/resources/js/index.js
@@ -37,21 +37,87 @@ $(document).ready(function () {
         $(this).css("color", "#666666")
     })
 
-    var prevHeight = 0;
-    $(".detail-nav .detail-nav-item").each(function () {
+    //var prevHeight = 0;
+    //$(".detail-nav .detail-nav-item").each(function () {
+    //
+    //    var height = $(this).offset().top;
+    //
+    //    if (prevHeight != height && prevHeight != 0) {
+    //
+    //        $(this).prev().find("a").css("border-right", "1px solid transparent");
+    //
+    //    }
+    //
+    //    prevHeight = height;
+    //
+    //
+    //})
 
-        var height = $(this).offset().top;
+    var totalWidth = 664;
+    var currentWidth = 0;
+    var tmp;
+    var json = {};
+    var xiabiao = 0;
+    var array = new Array();
+    $(".temp .detail-nav-item").each(function(idx, el){
 
-        if (prevHeight != height && prevHeight != 0) {
+        var width = $(this).width();
 
-            $(this).prev().find("a").css("border-right", "1px solid transparent");
+        tmp = width + currentWidth;
+        if(tmp > 664){
+
+            json[xiabiao++] = array;
+
+            array = new Array();
+
+            currentWidth = 0;
 
         }
 
-        prevHeight = height;
+        array.push(idx);
+
+        currentWidth += width;
+
+    });
+
+    for(var x in json){
+
+        var widthInOneLine = 664;
+
+        var array = json[x];
+
+        var widget = $("<div class='detail-nav'>");
+
+        for(var i=0;i<array.length;i++){
+
+            var index = array[i];
+
+            var oneLine = $(".temp .detail-nav-item")[index];
+
+            var oneWidth = $(oneLine).width();
+
+            widget.append(oneLine);
+
+            $(oneLine).removeClass("hide");
+
+            widthInOneLine = widthInOneLine - oneWidth;
+
+        }
+
+        widget.appendTo(".detail-content");
+
+        var size = array.length;
+
+        var n = widthInOneLine / (size*2);
+
+        var padding = "0 " + n + "px";
+
+        widget.find(".detail-nav-item a").css("padding", padding)
+
+    }
 
 
-    })
+
 
     $(".detail-nav .detail-nav-item:last").find("a").css("border-right", "1px solid transparent");
 
